5c828f3ea005ec040a979eb67a56cde2230a9ef2
Rimjhim <kumaririmjhim720@gmail.com>
2021-08-21 19:04:44 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import perform from './perform'
+import pic from './img/KVL.jpg'
+
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+     <script src="./exp.js" type="text/javascript">
+    </script>
+     <div id="whole_body" className=" w-10/12 h-auto ml-auto mr-auto ">
+       <div class="relative w-full">
+        <div class="  font-bold text-black px-4 rounded-lg text-5xl text-center h-auto mt-8 ">
+          Experiment: Verification of Kirchoff's Voltage Law
+        </div>
+        
+        </div>
+        <form name="f1" id="f1">
+          <div className="static w-full">
+            <br />
+            <div id="main_circuit" class="relative block w-7/12 mt-24">
+              <img src={pic } className=" absolute border-2 border-white w-full" alt="pic" />
+              <input type="number" name="r1" defaultValue={100} id="r1" className=" absolute w-12  left-64 ml-4 mt-28" />
+              <input type="number" name="r2" defaultValue={150} id="r2" className="absolute w-12 right-56  mt-28"/>
+              <input type="number" name="r3" defaultValue={200} id="r3" className="absolute w-11 left-72 -ml-2  mt-44" />
+              <input type="number" name="v2" defaultValue={110} id="v2" className=" absolute w-12 right-10 mt-44" />
+              <input type="number" name="v1" defaultValue={220} id="v1" className="absolute w-12 left-8 mt-44" />
+              <input type="number" name="V1" defaultValue={0} id="V1" className=" absolute left-44 top-60 text-center bg-gradient-to-r from-blue w-14 text-black border-2 border-blue rounded-lg" readOnly="readonly" />
+              <input type="number" name="V2" defaultValue={0} id="V2" className="absolute left-44 mt-1 text-center bg-gradient-to-r from-blue w-14 text-black border-2 border-blue rounded-lg" readOnly="readonly" />
+              <input type="number" name="V3" defaultValue={0} id="V3" className="absolute right-52 mt-1 text-center bg-gradient-to-r from-blue w-14 text-black border-2 border-blue rounded-lg" readOnly="readonly" />
+              <input type="number" name="V4" defaultValue={0} id="V4" className="absolute right-60 mr-10 mt-60 text-center bg-gradient-to-r from-blue w-14 text-black border-2 border-blue rounded-lg" readOnly="readonly" />
+              <input type="number" name="V5" defaultValue={0} id="V5" className="absolute right-24 mr-4 mt-60 text-center bg-gradient-to-r from-blue w-14 text-black border-2 border-blue rounded-lg" readOnly="readonly" />
+            </div>
+            <div className=" float-right p-4 w-96 h-92 border-4 border-black rounded-xl  ">
+              <div className=" font-bold text-3xl text-center">
+                Instructions
+              </div>
+              <br />
+              <div id="tab-content1" className="animated fadeIn">
+
+                <ul  class="list-disc p-4 text-xl text-blue italic ">
+                  <li class="mb-4">Input the values of Voltage sources &amp; Resistances </li>
+                  <li> Then click on Run.</li>
+                  </ul>
+               
+                <p className=" text-center"><input type="button" className="mt-12 mb-4 inline block text-center font-bold bg-gradient-to-r from-blue border-2 border-black  text-2xl px-2  rounded-2xl text-pink-800 " defaultValue="Verify" onClick={perform}/></p>
+              </div>
+            </div>
+          </div>
+          <div className=" text-center clear-both " />
+          <div id="observation" className=" text-center w-11/12 mt-28  ">
+            <h3 className="font-bold text-4xl  text-black mb-4 ">Observation Table</h3>
+            <table cellSpacing={0} className="text-sm border-4 border-black center" >
+              {/* cellspacing='0' is important, must stay */}
+              <tbody><tr  >
+                  <th class="bg-blue border-b-2 border-r-2 text-xl  border-black p-8" rowSpan={2}>SL No.</th>
+                  <th class="bg-blue border-b-2 border-r-2 text-xl  border-black" colSpan={5}>Voltmeter Readings</th>
+                  <th class="bg-blue border-b-2 border-r-2  text-xl  border-black" colSpan={2}>Verification of KVL </th>
+                </tr>
+                <tr>
+                  <th class="bg-blue border-b-2 border-r-2   border-black">V1</th>
+                  <th class="bg-blue border-b-2 border-r-2   border-black ">V2</th>
+                  <th class="bg-blue border-b-2 border-r-2   border-black ">V3</th>
+                  <th class="bg-blue border-b-2 border-r-2   border-black ">V4</th>
+                  <th class="bg-blue border-b-2 border-r-2   border-black">V5</th>
+                  <th class="bg-blue border-b-2 border-r-2   border-black">V2+V4 </th>
+                  <th class="bg-blue border-b-2 border-r-2   border-black">-V3+V4 </th>
+                </tr>
+                <tr>
+                  <td class="border-2 border-blue p-4 ">1</td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v11" defaultValue id="tr1" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v21" defaultValue id="tr2" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v31" defaultValue id="tr3" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v41" defaultValue id="tr4" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v51" defaultValue id="tr5" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k11" defaultValue id="tr6" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k21" defaultValue id="tr7" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                </tr>
+                {/* Table Row */}
+                <tr>
+                  <td class="border-2 border-blue p-4"><strong>2</strong></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v12" defaultValue id="tr1" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v22" defaultValue id="tr2" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v32" defaultValue id="tr3" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v42" defaultValue id="tr4" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v52" defaultValue id="tr5" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k12" defaultValue id="tr6" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"> <input type="number" name="k22" defaultValue id="tr7" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                </tr>
+                <tr>
+                  <td class="border-2 border-blue p-4"><strong>3</strong></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v13" defaultValue id="tr1" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v23" defaultValue id="tr2" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v33" defaultValue id="tr3" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v43" defaultValue id="tr4" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v53" defaultValue id="tr5" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k13" defaultValue id="tr6" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k23" defaultValue id="tr7" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                </tr>
+                <tr>
+                  <td class="border-2 border-blue p-4"><strong>4</strong></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v14" defaultValue id="tr1" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v24" defaultValue id="tr2" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v34" defaultValue id="tr3" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v44" defaultValue id="tr4" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v54" defaultValue id="tr5" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k14" defaultValue id="tr6" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k24" defaultValue id="tr7" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                </tr>
+                <tr>
+                  <td class="border-2 border-blue p-4"><strong>5</strong></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v15" defaultValue id="tr1" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v25" defaultValue id="tr2" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v35" defaultValue id="tr3" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v45" defaultValue id="tr4" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="v55" defaultValue id="tr5" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k15" defaultValue id="tr6" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                  <td class="border-2 border-blue p-4"><input type="number" name="k25" defaultValue id="tr7" className="bg-transparent text-black text-center border-none w-20" readOnly="readonly" /></td>
+                </tr>
+              </tbody></table>
+            <br />
+            <br />
+          </div>
+          <div className="mb-8 ml-96">
+            <input type="button" onclick="window.print()" defaultValue="Click here to print the observation table" className=" border-4 border-black w-auto p-2 font-bold bg-gradient-to-r from-blue text-pink-700	 text-xl   rounded-lg " />
+           
+          </div>
+        </form>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/perform.js
@@ -0,0 +1,115 @@
+import React from 'react'
+
+var count = 0;
+var dr1, dr2, dr3, dr4, dr5, dr6, dr7, dr8;
+
+export default function perform() {
+
+    
+
+    var r1, r2, r3, v1, v2;
+    var i1, i2, k1, k2, v3, v4, v5;
+    r1 = parseFloat(document.getElementById('r1').value);
+    r2 = parseFloat(document.getElementById('r2').value);
+    r3 = parseFloat(document.getElementById('r3').value);
+    v1 = parseFloat(document.getElementById('v1').value);
+    v2 = parseFloat(document.getElementById('v2').value);
+
+
+    i1 = (v1 * (r2 + r3) + r3 * v2) / ((r1 + r3) * (r2 + r3) - r3 * r3);
+    i2 = (v2 * (r1 + r3) + r3 * v1) / ((r1 + r3) * (r2 + r3) - r3 * r3);
+
+    v3 = i1 * r1;
+    v4 = (i1 - i2) * r3;
+    v5 = i2 * r2;
+
+    document.f1.V1.value = v1;
+    console.log("hi there"+v3);
+    document.f1.V5.value = v2;
+
+    document.f1.V2.value = v3;
+    document.f1.V3.value = v5;
+    document.f1.V4.value = v4;
+
+
+    count = count + 1;
+
+    if (count === 1) {
+
+        document.f1.v11.value = v1;
+      console.log( document.f1.v11.value) ;
+        document.f1.v21.value = v3;
+        document.f1.v31.value = v5;
+        document.f1.v41.value = v4;
+        document.f1.v51.value = v2;
+
+        document.f1.k11.value = v3 + v4;
+        document.f1.k21.value = v5 - v4;
+    } else if (count === 2) {
+
+        document.f1.v12.value = v1;
+        document.f1.v22.value = v3;
+        document.f1.v32.value = v5;
+        document.f1.v42.value = v4;
+        document.f1.v52.value = v2;
+
+        document.f1.k12.value = v3 + v4;
+        document.f1.k22.value = v5 - v4;
+    } else if (count === 3) {
+
+        document.f1.v13.value = v1;
+        document.f1.v23.value = v3;
+        document.f1.v33.value = v5;
+        document.f1.v43.value = v4;
+        document.f1.v53.value = v2;
+
+        document.f1.k13.value = v3 + v4;
+        document.f1.k23.value = v5 - v4;
+    } else if (count === 4) {
+
+        document.f1.v14.value = v1;
+        document.f1.v24.value = v3;
+        document.f1.v34.value = v5;
+        document.f1.v44.value = v4;
+        document.f1.v54.value = v2;
+
+        document.f1.k14.value = v3 + v4;
+        document.f1.k24.value = v5 - v4;
+    } else if (count === 5) {
+
+        document.f1.v15.value = v1;
+        document.f1.v25.value = v3;
+        document.f1.v35.value = v5;
+        document.f1.v45.value = v4;
+        document.f1.v55.value = v2;
+
+        document.f1.k15.value = v3 + v4;
+        document.f1.k25.value = v5 - v4;
+    } else
+        alert("The observation table is full. Refresh the page to continue");
+
+
+
+    if (count < 6)
+        alert('Observation table updated');
+
+    	// dr1=document.f1.A.value;
+    	// 			dr2=document.f1.A.value * rl1;
+    	// 		dr3=rl1;
+
+
+}
+
+
+const a = () => {
+    return (
+        <div>
+           {console.log("a fun")} 
+            {perform}
+        </div>
+    )
+}
+
+export {a};
+
+
